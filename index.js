be858e78f1177e2bb6201dc7dea0756ef7922601
mteachey <melindateachey@gmail.com>
2020-08-18 18:59:35 -0600

    refactored class names following BEM architecture

--- index.js
+++ index.js
@@ -1,62 +1,54 @@
 function handleClickToSeeWorkExamples(){
-    $('.js-toggle-buttons-container').on('click', '.js-toggle__button-work-all', function(event) {
+    $('.work-buttons').on('click', '.js-toggle__button-work-all', function(event) {
         $('.js-toggle__button-work').removeClass('button-active') ;
         $(this).addClass('button-active') ;
-        $('.work-container__example').addClass('js-toggle__display');
+        $('.work-example').addClass('js-toggle__display');
         //$('.example-1').addClass('js-toggle__display');  
       });   
-      $('.js-toggle-buttons-container').on('click', '.js-toggle__button-work-apps', function(event) {
+      $('.work-buttons').on('click', '.js-toggle__button-work-apps', function(event) {
         $('.js-toggle__button-work').removeClass('button-active') ;
         $(this).addClass('button-active') ;
-        $('.work-container__example').removeClass('js-toggle__display');
+        $('.work-example').removeClass('js-toggle__display');
         $('.js-type-app').addClass('js-toggle__display');  
       });     
-      $('.js-toggle-buttons-container').on('click', '.js-toggle__button-work-websites', function(event) {
+      $('.work-buttons').on('click', '.js-toggle__button-work-websites', function(event) {
         $('.js-toggle__button-work').removeClass('button-active') ;
         $(this).addClass('button-active') ;
-        $('.work-container__example').removeClass('js-toggle__display');
+        $('.work-example').removeClass('js-toggle__display');
         $('.js-type-website').addClass('js-toggle__display');  
       });     
-      $('.js-toggle-buttons-container').on('click', '.js-toggle__button-work-react', function(event) {
+      $('.work-buttons').on('click', '.js-toggle__button-work-react', function(event) {
         $('.js-toggle__button-work').removeClass('button-active') ;
         $(this).addClass('button-active') ;
-        $('.work-container__example').removeClass('js-toggle__display');
+        $('.work-example').removeClass('js-toggle__display');
         $('.js-type-react').addClass('js-toggle__display');  
       });     
-      $('.js-toggle-buttons-container').on('click', '.js-toggle__button-work-fullstack', function(event) {
+      $('.work-buttons').on('click', '.js-toggle__button-work-fullstack', function(event) {
         $('.js-toggle__button-work').removeClass('button-active') ;
         $(this).addClass('button-active') ;
-        $('.work-container__example').removeClass('js-toggle__display');
+        $('.work-example').removeClass('js-toggle__display');
         $('.js-type-fullstack').addClass('js-toggle__display');  
       });     
       
 }
 
 function MobileMenuAnimation(){        
-    $('.header__mobile-menu-icon-container').on('click',function(event) {
+    $('.mobile-menu-icon-container').on('click',function(event) {
         $(this).toggleClass('change');  
-        $('.nav').toggleClass('mobile-nav');
+        $('nav').toggleClass('mobile-nav');
     });   
-    $('.nav').on('click', function(event) {
-        $('.header__mobile-menu-icon-container').removeClass('change');  
-        $('.nav').removeClass('mobile-nav');
+    $('nav').on('click', function(event) {
+        $('.mobile-menu-icon-container').removeClass('change');  
+        $('nav').removeClass('mobile-nav');
     });        
 }
 
 
 function activeNavLinks(){
-    $('.nav__list-item--home').on('click', function(event) {
-        $('.nav__list-item').removeClass('link-active') ;
+    $('.nav-link').on('click', function(event) {
+        $('.nav-link').removeClass('link-active') ;
         $(this).addClass('link-active') ;
       });   
-    $('.js-active--home').on('click',function(event){
-      $('.nav__list-item').removeClass('link-active');
-      $('.nav__list-item--home-link').addClass('link-active')
-    });
-    $('.js-active--contact').on('click',function(event){
-      $('.nav__list-item').removeClass('link-active');
-      $('.nav__list-item--contact-link').addClass('link-active')
-    })
 }
 
 function navScrollEffect() {
@@ -77,7 +69,7 @@ function navScrollEffect() {
     }); 
 }
 
-$('.js-smooth-scroll[href*="#"]').on('click', function(e) {
+$('.work-av-link[href*="#"]').on('click', function(e) {
     e.preventDefault();
     $('.blue-dots,.green-dots,.orange-dots').addClass('js-stop-animation');
   
